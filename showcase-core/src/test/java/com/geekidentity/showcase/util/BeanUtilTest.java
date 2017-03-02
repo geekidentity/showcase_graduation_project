637f22ace275c3d729dbe297aa79355aba15e7e3
geekidentity <geekidentity@163.com>
2017-03-02 10:42:39 +0800

    add getCamelNameByUnderline test

---
+++ showcase-core/src/test/java/com/geekidentity/showcase/util/BeanUtilTest.java
@@ -0,0 +1,15 @@
+package com.geekidentity.showcase.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BeanUtilTest {
+	
+	@Test
+	public void getCamelNameTest() {
+		String underlineName = "get_User_name";
+		String result = BeanUtil.getCamelNameByUnderline(underlineName);
+		assertEquals("getUserName", result);
+	}
+}
